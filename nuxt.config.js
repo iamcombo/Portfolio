04f92895758e243e5ba6840a11843a6e809d9b63
Piset Heang <piset2cambo@gmail.com>
2020-07-05 20:43:59 +0700

    add lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ export default {
   */
   head: {
     title: 'Portfolio',
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
